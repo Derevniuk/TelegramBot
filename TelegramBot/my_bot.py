6ee2427ab04f6f39460c4f354955787337883536
Peter Derevniuk <derevnukp@gmail.com>
2022-07-11 02:34:12 +0300

    Add function
    Organized functions, alarm clock, timer, notes

--- TelegramBot/my_bot.py
+++ TelegramBot/my_bot.py
@@ -1,52 +1,71 @@
 from telebot import types
 from settnigs import bot
-from services import my_timer, my_alarm
+from services import my_timer, my_alarm, time_calculation
 
+bd = []
 
 @bot.message_handler(commands=['start'])
 def start(message):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    btn1 = types.KeyboardButton("Будильник")
-    btn2 = types.KeyboardButton("Таймер")
-    markup.add(btn1, btn2)
-    bot.send_message(message.chat.id, text="Я твой личный органайзер. Выбери что необходимо сделать".format(message.from_user), reply_markup=markup)
+    btn_1 = types.KeyboardButton("Alarm")
+    btn_2 = types.KeyboardButton("Timer")
+    btn_3 = types.KeyboardButton("Notes")
+    markup.add(btn_1, btn_2, btn_3)
+    bot.send_message(message.chat.id, text="I am your personal organizer. Choose what needs to be done.".format(message.from_user), reply_markup=markup)
+
+
+@bot.message_handler(commands=['next_start'])
+def next_star(message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    btn_1 = types.KeyboardButton("Alarm")
+    btn_2 = types.KeyboardButton("Timer")
+    btn_3 = types.KeyboardButton("Notes")
+    markup.add(btn_1, btn_2, btn_3)
+    bot.send_message(message.chat.id,
+                     text="Main menu".format(message.from_user), reply_markup=markup)
 
 
 @bot.message_handler(content_types=['text'])
-def get_text_messages(message):
-    if message.text == 'Таймер':
+def get_timer(message):
+    if message.text == 'Timer':
         markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        btn1 = types.KeyboardButton("1 минута")
-        btn2 = types.KeyboardButton("2 минуты")
-        btn3 = types.KeyboardButton("5 минут")
-        btn4 = types.KeyboardButton("10 минут")
-        btn5 = types.KeyboardButton("30 минут")
-        btn6 = types.KeyboardButton("Назад")
-        markup.add(btn1, btn2, btn3, btn4, btn5, btn6)
-        bot.send_message(message.chat.id, text="Выбери время таймера, либо введи вручную(в минутах)".format(message.from_user), reply_markup=markup)
-    if message.text == '1 минута':
+        btn_1 = types.KeyboardButton("1 minute")
+        btn_2 = types.KeyboardButton("2 minute")
+        btn_3 = types.KeyboardButton("5 minute")
+        btn_4 = types.KeyboardButton("10 minute")
+        btn_5 = types.KeyboardButton("30 minute")
+        btn_6 = types.KeyboardButton("Main menu")
+        markup.add(btn_1, btn_2, btn_3, btn_4, btn_5, btn_6)
+        bot.send_message(message.chat.id, text="Select the timer time, or enter manually (in minutes)"
+                         .format(message.from_user), reply_markup=markup)
+    input_text = message.text.split(':')
+    if len(input_text) == 1 and input_text[0].isdigit():
+        add_hour, add_minutes = time_calculation(int(input_text[0]))
+        print(add_hour, add_minutes)
+        my_timer(message, add_minutes, add_hour)
+    if message.text == '1 minute':
         my_timer(message, 1, 0)
-    if message.text == '2 минуты':
+    if message.text == '2 minute':
         my_timer(message, 2, 0)
-    if message.text == '5 минут':
+    if message.text == '5 minute':
         my_timer(message, 5, 0)
-    if message.text == '10 минут':
+    if message.text == '10 minute':
         my_timer(message, 10, 0)
-    if message.text == '30 минут':
+    if message.text == '30 minute':
         my_timer(message, 30, 0)
-    if message.text == 'Назад':
-        start(message)
-    if message.text == "Будильник":
+    if message.text == 'Main menu':
+        next_star(message)
+    if message.text == "Alarm":
         markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        btn1 = types.KeyboardButton("06:00")
-        btn2 = types.KeyboardButton("07:00")
-        btn3 = types.KeyboardButton("08:00")
-        btn4 = types.KeyboardButton("09:00")
-        btn5 = types.KeyboardButton("14:00")
-        btn6 = types.KeyboardButton("Назад")
-        markup.add(btn1, btn2, btn3, btn4, btn5, btn6)
+        btn_1 = types.KeyboardButton("06:00")
+        btn_2 = types.KeyboardButton("07:00")
+        btn_3 = types.KeyboardButton("08:00")
+        btn_4 = types.KeyboardButton("09:00")
+        btn_5 = types.KeyboardButton("14:00")
+        btn_6 = types.KeyboardButton("Main menu")
+        markup.add(btn_1, btn_2, btn_3, btn_4, btn_5, btn_6)
         bot.send_message(message.chat.id,
-                         text="Выбери время будильника, либо введи вручную(часы, минуты)".format(message.from_user),
+                         text="Choose the alarm time, or enter manually (hours, minutes)".format(message.from_user),
                          reply_markup=markup)
     if message.text == '06:00':
         my_alarm(message, 0, 6)
@@ -58,8 +77,45 @@ def get_text_messages(message):
         my_alarm(message, 0, 9)
     if message.text == '14:00':
         my_alarm(message, 0, 14)
-    if message.text == 'Назад':
-        start(message)
+    if message.text == "Notes":
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        btn_1 = types.KeyboardButton("Add note")
+        btn_2 = types.KeyboardButton("Edit note")
+        btn_3 = types.KeyboardButton("Show notes")
+        btn_4 = types.KeyboardButton("Delete note")
+        btn_5 = types.KeyboardButton("Main menu")
+        markup.add(btn_1, btn_2, btn_3, btn_4, btn_5)
+        bot.send_message(message.chat.id,
+                         text="Choose your course of action".format(message.from_user),
+                         reply_markup=markup)
+    if message.text == "Add note":
+        bot.send_message(message.from_user.id, 'To add a note, type: add_(note content)')
+    if message.text.split('_')[0].lower() == 'add':
+        bd.append(message.text[4:])
+        bot.send_message(message.from_user.id, f'Added {len(bd)} note')
+    if message.text == "Show notes":
+        for one_bd in bd:
+            bot.send_message(message.from_user.id, f'{bd.index(one_bd) + 1}. {one_bd}')
+    if message.text == "Delete note":
+        bot.send_message(message.from_user.id, 'To delete a note write(Del_number note)')
+    if message.text.split('_')[0].lower() == 'del':
+        if message.text.split('_')[1].isdecimal() and int(message.text.split('_')[1]) <= len(bd) and \
+                int(message.text.split('_')[1]) != 0:
+            stung_note_index = int(message.text.split('_')[1])
+            bd.pop(stung_note_index - 1)
+            bot.send_message(message.from_user.id, f'Note number {stung_note_index} deleted')
+        else:
+            bot.send_message(message.from_user.id, 'Enter the correct value!!!')
+    if message.text == "Edit note":
+        bot.send_message(message.from_user.id, 'For note names, type(edi_(note number)_(edited note text)))')
+    if message.text.split('_')[0].lower() == 'edi':
+        if message.text.split('_')[1].isdecimal() and int(message.text.split('_')[1]) <= len(bd) and \
+                int(message.text.split('_')[1]) != 0:
+            stung_note_index = int(message.text.split('_')[1])
+            bd[int(message.text.split('_')[1]) - 1] = (message.text.split('_')[2])
+            bot.send_message(message.from_user.id, f'Note number {stung_note_index} changed')
+        else:
+            bot.send_message(message.from_user.id, 'Enter the correct value!!!')
 
 
 bot.polling(none_stop=True, interval=0)
--- TelegramBot/services.py
+++ TelegramBot/services.py
@@ -7,11 +7,11 @@ def my_timer(message, add_min, add_hour):
     minutes = str(int(datetime.now().minute) + add_min)
     hour = str(datetime.now().hour + add_hour)
     second = str(datetime.now().second)
-    bot.send_message(message.from_user.id, 'Таймер установлен')
+    bot.send_message(message.from_user.id, 'Timer set')
     while True:
         if str(datetime.now().hour) == hour and str(datetime.now().minute) == minutes and str(
                 datetime.now().second) == second:
-            bot.send_message(message.from_user.id, 'Таймер')
+            bot.send_message(message.from_user.id, 'Timer')
             break
         sleep(1)
 
@@ -24,11 +24,27 @@ def my_alarm(message, add_min, add_hour):
         add_min = '0' + str(add_min)
     if add_hour < 10:
         add_hour = '0' + str(add_hour)
-    bot.send_message(message.from_user.id, f'Будильник установлен на {add_hour}:{add_min}')
+    bot.send_message(message.from_user.id, f'Alarm clock set to {add_hour}:{add_min}')
 
     while True:
         if str(datetime.now().hour) == hour and str(datetime.now().minute) == minutes and str(
                 datetime.now().second) == second:
-            bot.send_message(message.from_user.id, 'Будильник')
+            bot.send_message(message.from_user.id, 'Alarm!!!')
             break
         sleep(1)
+
+
+"""minutes validator"""
+
+
+def time_calculation(total_minute: int):
+    if total_minute > 60:
+        extra_hours = total_minute // 60
+        extra_minute = total_minute - 60 * extra_hours
+        return extra_hours, extra_minute
+    else:
+
+        return 0, total_minute
+
+
+
